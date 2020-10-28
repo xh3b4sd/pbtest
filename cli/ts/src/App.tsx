debcf3d48feed7ac750d165c259cf1e9348a7173
Marcus Ellison <marcus@venturemark.co>
2020-10-28 11:51:22 -0700

    scaffolding

---
+++ cli/ts/src/App.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import * as grpcWeb from "grpc-web";
+import { APIClient } from "./api/ApiServiceClientPb";
+import { CreateI, CreateO } from "./api/create_pb";
+
+// What is going on here?
+const client = new APIClient("http://localhost:8080");
+
+function App() {
+  console.log("our api:", client);
+
+  const request = new CreateI();
+  request.setName("Marcus");
+
+  client.create(request, {}, (err: grpcWeb.Error, response: CreateO) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(response);
+    }
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
